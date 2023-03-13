a3bbcb893e61dc058d18b699ce6304dbaef0e6d2
James Hodgkinson <james@terminaloutcomes.com>
2023-03-13 14:05:47 +1000

    adding/fixing some scripts for freeradius dev

--- platform/debian/install_deps.sh
+++ platform/debian/install_deps.sh
@@ -4,14 +4,15 @@ set -e
 
 echo "Updating local packages"
 if [ "$(whoami)" == "root" ]; then
-    apt-get update
+    SUDOCMD=""
 else
-    sudo apt-get update
+    SUDOCMD="sudo"
 fi
+${SUDOCMD} apt-get update
 
 echo "Installing build dependencies"
-if [ "$(whoami)" == "root" ]; then
-apt-get install -y \
+
+${SUDOCMD} apt-get install -y \
           libpam0g-dev \
           libudev-dev \
           libssl-dev \
@@ -22,19 +23,6 @@ apt-get install -y \
           fakeroot \
           dh-make \
           debmake
-else
-    sudo apt-get install -y \
-            libpam0g-dev \
-            libudev-dev \
-            libssl-dev \
-            libsqlite3-dev \
-            pkg-config \
-            make \
-            devscripts \
-            fakeroot \
-            dh-make \
-            debmake
-fi
 
 if [ -f "$HOME/.cargo/env" ]; then
     # shellcheck disable=SC1091
@@ -43,7 +31,7 @@ fi
 
 if [ "$(which cargo | wc -l)" -ne 1 ]; then
     echo "Installing rust"
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > "$TMPDIR/rustup.sh"
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > "${TMPDIR}/rustup.sh"
     chmod +x "${TMPDIR}/rustup.sh"
     "${TMPDIR}/rustup.sh" -y
     echo "Done installing rust!"
@@ -56,11 +44,7 @@ if  [ "$1" == "kanidmd" ] && [ "$(which wasm-pack | wc -l)" -eq 0 ]; then
 	echo "Downloading script to ~/install-wasm-pack"
 	curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf > "${HOME}/install-wasm-pack"
 	chmod +x "${HOME}/install-wasm-pack"
-    if [ "$(whoami)" == "root" ]; then
-        "${HOME}/install-wasm-pack"
-    else
-        sudo "${HOME}/install-wasm-pack"
-    fi
+    ${SUDOCMD} "${HOME}/install-wasm-pack"
     rm "${HOME}/install-wasm-pack"
 else
     echo "wasm-pack already installed"
