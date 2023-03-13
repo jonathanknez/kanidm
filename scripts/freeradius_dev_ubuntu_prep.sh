4fa0ed837174ea71c7459a5ecef7251480861c4b
James Hodgkinson <james@terminaloutcomes.com>
2023-03-13 18:50:25 +1000

    more updates to the freeradius-prepare

--- scripts/freeradius_dev_ubuntu_prep.sh
+++ scripts/freeradius_dev_ubuntu_prep.sh
@@ -13,7 +13,6 @@ if [ -z "$(find /mnt/shared/  -maxdepth 1 -type f | head)" ]; then
         echo "Adding shared folder to fstab"
         echo 'share	/mnt/shared	9p	trans=virtio,version=9p2000.L,rw,_netdev,nofail	0	0' | ${SUDOCMD} tee -a /etc/fstab
     fi
-
     ${SUDOCMD} mount /mnt/shared
 fi
 
@@ -21,5 +20,30 @@ fi
 echo "Installing dependencies..."
 ../platform/debian/install_deps.sh || exit 1
 
+if [ -z "${CARGO_TARGET_DIR}" ]; then
+    # shellcheck disable=SC2016
+    echo 'setting cargo target env var (CARGO_TARGET_DIR) to $HOME/target via /etc/profile'
+    # shellcheck disable=SC2016
+    echo 'export CARGO_TARGET_DIR="$HOME/target"' | ${SUDOCMD} tee -a /etc/profile
+fi
 
-${SUDOCMD} apt-get -y install freeradius freeradius-utils libfreeradius-dev python-is-python3
+${SUDOCMD} apt-get -y install \
+    python-is-python3 \
+    ripgrep\
+    libtalloc-dev \
+    libkqueue-dev \
+    libcurl4-openssl-dev \
+    libjson-c-dev \
+    jq
+
+MYDIR="$(basedir "$@")"
+
+cd "${HOME}" || exit 1
+
+"${MYDIR}/scripts/get-latest-freeradius-release.sh"
+
+FRS_DIR="$(find "${HOME}" -maxdepth 1 -type d -name 'freeradius-server-*')"
+
+cd "${FRS_DIR}" || exit 1
+./configure --enable-developer || exit 1
+make
